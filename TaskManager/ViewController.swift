72d0aead35407401730997023bb03eb2416116fe
Rohith <r12.rohith@gmail.com>
2018-05-24 12:52:04 -0400

    Add new item functionality completed.

--- TaskManager/ViewController.swift
+++ TaskManager/ViewController.swift
@@ -21,14 +21,51 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - Action
+    
+    @IBAction func addItem(_ sender: Any) {
+       
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Add new task items", message: "", preferredStyle: .alert)
+       
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            if let text = textfield.text{
+                self.items.append(text)
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+        
+            alertTextField.placeholder = "Create new item"
+            textfield = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    // MARK: - Tableview delegates
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return items.count
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
         cell.textLabel?.text = items[indexPath.row]
+        
         return cell
     }
 
